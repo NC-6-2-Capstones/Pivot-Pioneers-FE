c5b09c2f4f145c8737e48ddb679a5fc414d1dc18
tetsurugan <kukura2@gmail.com>
2025-05-02 13:08:43 -0400

    feat: implement routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { ChakraProvider, Box } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
+// Import your page components
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
+import GoalFormPage from './pages/GoalFormPage';
 
 function App() {
   return (
     <ChakraProvider>
-      <Box>
-        <Navbar />
-        <DashboardPage />
-      </Box>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/goal" element={<GoalFormPage />} />
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 }
 
-export default App
+export default App;
